fdb70cfd0d0c5e380717fd51a8d16a9525b29504
Jitender Shekhawat <jitendersinghshekhawat@gmail.com>
2020-10-06 20:02:05 +0530

    add demo config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import CodeMirrorDep from 'codemirror'
 
 
 const CodeMirror = ({}) => {
-
+    const editorInst = useState()
     const editorRef = useRef()
         
 
