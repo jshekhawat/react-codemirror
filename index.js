88441fb717d5a7fb181b2617e3216998775cf4ac
Jitender Shekhawat <jitendersinghshekhawat@gmail.com>
2020-10-13 16:01:04 +0530

    add value updation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react'
+import React, {useEffect, useMemo, useRef, useState} from 'react'
 import CodeMirrorDep from 'codemirror'
 import loadMode from './modes'
 import 'codemirror/mode/meta'
@@ -6,19 +6,20 @@ import './codemirror.css'
 
 
 const CodeMirror = (props) => {
-    const {width, height, mode, ...options} = props
+    const {width, height, mode, value, ...options} = props
     const [editorInst, setInstance] = useState()
-    const editorRef = useRef()    
+    const editorRef = useRef()       
 
     useEffect(() => {        
         if (!editorInst) {            
             const editor = CodeMirrorDep.fromTextArea(editorRef.current, {...options})            
-            editor.setSize(width, height)            
+            editor.setSize(width, height)
             if (mode) {
                 const codeMode = CodeMirrorDep.findModeByName(mode)                           
                 loadMode(codeMode)
                 editor.setOption("mode", codeMode.mime)
-            }
+            }            
+            editor.setValue(value)              
             Object.keys(options).forEach(key => {
                 editor.setOption(key, options[key])
             })
@@ -33,6 +34,13 @@ const CodeMirror = (props) => {
             }          
         }
     }, [])
+
+    useMemo(() => {
+        if(!editorInst) return
+        if (!!value && value !== editorInst.value) {
+            editorInst.setValue(value)
+        } 
+    }, [value])
 
     return (<textarea ref={editorRef}/>)
 }
